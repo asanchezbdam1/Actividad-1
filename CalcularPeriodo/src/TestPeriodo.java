940f1e6540504fe6d4ae2fa8bcae0c0d0c408409
asanchezbdam1 <asanchezbdam1@gmail.com>
2021-05-25 21:38:32 +0200

    Modificada la case TestPeriodo

--- CalcularPeriodo/src/TestPeriodo.java
+++ CalcularPeriodo/src/TestPeriodo.java
@@ -3,6 +3,8 @@ public class TestPeriodo {
 
 	public static void main(String[] args) {
 		CalcularPeriodo.calcularPeriodo("Normandía", java.time.LocalDate.of(1944, 6, 6));
+		CalcularPeriodo.calcularPeriodo("Nuevo milenio", java.time.LocalDate.of(2000, 1, 1));
+		CalcularPeriodo.calcularPeriodo("Nacimiento de un tal Jesús o algo", java.time.LocalDate.of(0, 1, 1));
 	}
 
 }
